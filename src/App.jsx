ab1553cada001263ead5bb934c073587c689752b
MatWarm <mttwarmee@gmail.com>
2024-07-25 15:28:45 +0200

    ajout login + créeation de compte

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 // import { createTheme } from '@mui/material/styles';
 import { Drawer, List, ListItem, ListItemText, CssBaseline, Box } from '@mui/material';
 import Annonces from './pages/Annonces';
+import Login from './pages/Login';
+import CreeCompte from "./pages/CreeCompte";
 // import Profil from './Profil';
 // import MesReservations from './MesReservations';
 // import AjouterAnnonce from './AjouterAnnonce';
@@ -36,7 +38,14 @@ function App() {
               <ListItem button component={Link} to="/ajouter-annonce">
                 <ListItemText primary="Ajouter annonce" />
               </ListItem>
+              <ListItem button component={Link} to="/connexion">
+                <ListItemText primary="Connexion" />
+              </ListItem>
+              <ListItem button component={Link} to="/cree-compte">
+                <ListItemText primary="Nous rejoindre" />
+              </ListItem>
             </List>
+
           </Drawer>
           <Box
             component="main"
@@ -44,6 +53,8 @@ function App() {
           >
             <Routes>
               <Route path="/annonces" element={<Annonces />} />
+              <Route path="/connexion" element={<Login/>} />
+              <Route path="/cree-compte" element={<CreeCompte/>} />
               {/* <Route path="/profil" element={<Profil />} /> */}
               {/* <Route path="/mes-reservations" element={<MesReservations />} /> */}
               {/* <Route path="/ajouter-annonce" element={<AjouterAnnonce />} /> */}
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,26 +1,101 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
+import Cookies from 'js-cookie'
 
 function Login() {
-  return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      justifyContent="center"
-      minHeight="100vh"
-      p={3}
-    >
-      <h1>Connexion</h1>
-      <TextField label="Nom d'utilisateur" variant="outlined" margin="normal" />
-      <TextField label="Mot de passe" type="password" variant="outlined" margin="normal" />
-      <Button variant="contained" color="primary" style={{ marginTop: '16px' }}>
-        Se connecter
-      </Button>
-    </Box>
-  );
+    // État pour gérer les valeurs des champs de texte
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    // Fonction de gestionnaire d'événements pour le bouton
+    const handleClick = async () => {
+        // Créez l'objet avec les données de connexion
+        const loginData = {
+            mail: username,
+            password: password
+        };
+        const url = 'http://localhost:3001';
+
+        try {
+            // Appel à l'API avec fetch
+            console.log(loginData)
+            const response = await fetch(url+"/compte/login", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(loginData)
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                Cookies.set('token', data.token,{expires: 1});
+                console.log('Connexion réussie:', data);
+
+                console.log(url+"/compte/"+username)
+                const getInfoUtile = await fetch(url+"/compte/"+username, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization':Cookies.get('token')
+                    }
+                });
+
+                if(getInfoUtile.ok) {
+                    const data = await getInfoUtile.json();
+                    console.log(data);
+                    Cookies.set('mail',data.mail,{expires: 1})
+                    Cookies.set('id',data.id,{expires: 1})
+                    Cookies.set('nom',data.nom,{expires: 1})
+                    Cookies.set('prenom',data.prenom,{expires: 1})
+                }else{
+                    console.log("echec")
+                }
+            } else {
+                console.error('Erreur de connexion:', response.statusText);
+            }
+        } catch (error) {
+            console.error('Erreur de réseau:', error);
+        }
+    };
+
+    return (
+        <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            minHeight="100vh"
+            p={3}
+        >
+            <h1>Connexion</h1>
+            <TextField
+                label="Nom d'utilisateur"
+                variant="outlined"
+                margin="normal"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <TextField
+                label="Mot de passe"
+                type="password"
+                variant="outlined"
+                margin="normal"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+                variant="contained"
+                color="primary"
+                style={{ marginTop: '16px' }}
+                onClick={handleClick}
+            >
+                Se connecter
+            </Button>
+        </Box>
+    );
 }
 
 export default Login;
